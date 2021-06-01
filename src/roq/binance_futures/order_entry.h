58349692843e2fcc087b13daed2ddb6ed57196b2
thraneh <thraneh@gmail.com>
2021-06-01 15:29:40 +0200

    preparing for proper (fix-style) request-id chaining

--- src/roq/binance_futures/order_entry.h
+++ src/roq/binance_futures/order_entry.h
@@ -74,9 +74,16 @@ class OrderEntry final : public core::web::Client::Handler {
 
   uint16_t operator()(const Event<CreateOrder> &, const std::string_view &request_id);
   uint16_t operator()(
-      const Event<ModifyOrder> &, const std::string_view &request_id, const server::OMS_Order &);
+      const Event<ModifyOrder> &,
+      const server::Order &,
+      const std::string_view &request_id,
+      const std::string_view &previous_request_id);
   uint16_t operator()(
-      const Event<CancelOrder> &, const std::string_view &request_id, const server::OMS_Order &);
+      const Event<CancelOrder> &,
+      const server::Order &,
+      const std::string_view &request_id,
+      const std::string_view &previous_request_id);
+
   uint16_t operator()(const Event<CancelAllOrders> &);
 
   void get_depth(
@@ -108,8 +115,8 @@ class OrderEntry final : public core::web::Client::Handler {
 
   void cancel_order(
       const CancelOrder &,
+      const server::Order &,
       const std::string_view &request_id,
-      const server::OMS_Order &,
       std::function<void(const core::Promise<json::CancelOrder> &)> &&);
 
   void operator()(const json::NewOrder &);

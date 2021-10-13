542bc70958bb8f24dbb96c0dcdbf5a07ce4b5663
thraneh <thraneh@gmail.com>
2021-10-13 08:39:01 +0200

    prod

---
+++ src/roq/binance_futures/rencap.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+CWD="$(realpath "$(dirname "${BASH_SOURCE[0]}")")"
+
+if [ "$1" == "debug" ]; then
+	PREFIX="gdb --args"
+else
+	PREFIX=
+fi
+
+NAME="rencap"
+
+CONFIG_FILE="$CWD/config/$NAME.toml"
+
+URI="binance.com"
+
+REST_URI="https://fapi.$URI"
+WS_URI="wss://fstream.$URI/ws"
+
+$PREFIX ./roq-binance-futures \
+	--name "$NAME" \
+	--config_file "$CONFIG_FILE" \
+	--client_listen_address $CWD/$NAME.sock \
+	--metrics_listen_address $CWD/${NAME}_metrics.sock \
+	--ws_uri "$WS_URI" \
+	--rest_uri "$REST_URI" \
+	$@

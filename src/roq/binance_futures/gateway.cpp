768a25e85fdaa3c9ef7c5f8e4cabca7c075953b8
thraneh <thraneh@gmail.com>
2022-07-16 15:32:01 +0200

    allow io::Context to be created with requiring the libevent headers

--- src/roq/binance_futures/gateway.cpp
+++ src/roq/binance_futures/gateway.cpp
@@ -12,7 +12,7 @@
 #include "roq/core/clock.hpp"
 #include "roq/core/utils.hpp"
 
-#include "roq/io/engine/libevent/context_factory.hpp"
+#include "roq/io/engine/context_factory.hpp"
 
 #include "roq/binance_futures/flags.hpp"
 
@@ -68,7 +68,7 @@ auto create_drop_copy(auto &security_by_account) {
 
 Gateway::Gateway(server::Dispatcher &dispatcher, Config const &config)
     : dispatcher_(dispatcher), security_(create_security<decltype(security_)>(config)),
-      context_(io::engine::libevent::ContextFactory::create()), shared_(dispatcher),
+      context_(io::engine::ContextFactory::create_libevent()), shared_(dispatcher),
       request_(create_request<decltype(request_)>(config)), rest_(*this, *context_, ++stream_id_, shared_),
       order_entry_(
           create_order_entry<decltype(order_entry_)>(*this, *context_, stream_id_, security_, shared_, request_)),

ac496180b49397fa1981289f9071c53c806f190b
thraneh <thraneh@gmail.com>
2022-07-26 13:26:06 +0200

    API changes needed to support drop-copy (#254)

--- src/roq/binance_futures/drop_copy.cpp
+++ src/roq/binance_futures/drop_copy.cpp
@@ -281,6 +281,7 @@ void DropCopy::operator()(Trace<json::OrderTradeUpdate const> const &event) {
                 .fills = {&fill, 1},
                 .routing_id = order.routing_id,
                 .update_type = {},
+                .user = {},
             };
             create_trace_and_dispatch(handler_, trace_info, trade_update, true, order.user_id);
           }

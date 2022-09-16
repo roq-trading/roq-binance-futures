805f43f5afaf81c3ed10c0b422bd68b68cc59286
thraneh <thraneh@gmail.com>
2022-09-16 14:47:58 +0200

    io context in server

--- src/roq/binance_futures/gateway.cpp
+++ src/roq/binance_futures/gateway.cpp
@@ -12,8 +12,6 @@
 #include "roq/core/clock.hpp"
 #include "roq/core/utils.hpp"
 
-#include "roq/io/engine/context_factory.hpp"
-
 #include "roq/binance_futures/flags.hpp"
 
 #include "roq/binance_futures/json/utils.hpp"
@@ -66,12 +64,11 @@ auto create_drop_copy(auto &security_by_account) {
 }
 }  // namespace
 
-Gateway::Gateway(server::Dispatcher &dispatcher, Config const &config)
-    : dispatcher_(dispatcher), security_(create_security<decltype(security_)>(config)),
-      context_(io::engine::ContextFactory::create_libevent()), shared_(dispatcher),
-      request_(create_request<decltype(request_)>(config)), rest_(*this, *context_, ++stream_id_, shared_),
-      order_entry_(
-          create_order_entry<decltype(order_entry_)>(*this, *context_, stream_id_, security_, shared_, request_)),
+Gateway::Gateway(server::Dispatcher &dispatcher, Config const &config, io::Context &context)
+    : dispatcher_(dispatcher), security_(create_security<decltype(security_)>(config)), context_(context),
+      shared_(dispatcher), request_(create_request<decltype(request_)>(config)),
+      rest_(*this, context_, ++stream_id_, shared_), order_entry_(create_order_entry<decltype(order_entry_)>(
+                                                         *this, context_, stream_id_, security_, shared_, request_)),
       drop_copy_(create_drop_copy<decltype(drop_copy_)>(security_)) {
   if (Flags::rest_cancel_on_disconnect())
     log::fatal("Exchange does *NOT* support cancel on disconnect"sv);
@@ -109,7 +106,6 @@ void Gateway::operator()(Event<Timer> const &event) {
       (*drop_copy)(event);
   for (auto &market_data : market_data_)
     (*market_data)(event);
-  (*context_).drain();
 }
 
 void Gateway::operator()(Event<Connected> const &) {
@@ -200,7 +196,7 @@ void Gateway::ensure_symbol_slices(size_t size) {
     auto stream_id = ++stream_id_;
     auto index = std::size(market_data_);
     log::debug("Create MarketData (stream_id={}, index={})"sv, stream_id, index);
-    auto market_data = std::make_unique<MarketData>(*this, *context_, stream_id_, shared_, index);
+    auto market_data = std::make_unique<MarketData>(*this, context_, stream_id_, shared_, index);
     MessageInfo message_info;
     const Start start;
     create_event_and_dispatch(*market_data, message_info, start);
@@ -217,7 +213,7 @@ void Gateway::operator()(OrderEntry::ListenKeyUpdate const &listen_key_update) {
   } else if (!static_cast<bool>((*iter).second)) {
     log::info(R"(Create drop-copy (user-stream) for account="{}")"sv, account);
     auto drop_copy = std::make_unique<DropCopy>(
-        *this, *context_, ++stream_id_, *security_[account], shared_, request_[account], listen_key_update.listen_key);
+        *this, context_, ++stream_id_, *security_[account], shared_, request_[account], listen_key_update.listen_key);
     MessageInfo message_info;
     const Start start;
     create_event_and_dispatch(*drop_copy, message_info, start);

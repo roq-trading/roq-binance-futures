249ba019c0a6b725fe77796fcbc47f9862b1bcbe
thraneh <thraneh@gmail.com>
2022-10-23 15:34:18 +0200

    chore

--- src/roq/binance_futures/api.cpp
+++ src/roq/binance_futures/api.cpp
@@ -39,7 +39,7 @@ API API::create() {
         .countdown_cancel_all = "/fapi/v1/countdownCancelAll"sv,
     };
   }
-  throw RuntimeError(R"(Unknown api="{}")"sv, api);
+  throw RuntimeError{R"(Unknown api="{}")"sv, api};
 }
 
 }  // namespace binance_futures

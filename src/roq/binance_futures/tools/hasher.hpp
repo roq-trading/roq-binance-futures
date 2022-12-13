0b8347f91b9dbc04a56bc4250895e50ad3147304
thraneh <thraneh@gmail.com>
2022-12-13 12:36:49 +0100

    refactor crypto->{hash,mac}

--- src/roq/binance_futures/tools/hasher.hpp
+++ src/roq/binance_futures/tools/hasher.hpp
@@ -7,7 +7,7 @@
 #include <string_view>
 #include <utility>
 
-#include "roq/core/crypto/hmac_sha256.hpp"
+#include "roq/core/mac/hmac_sha256.hpp"
 
 namespace roq {
 namespace binance_futures {
@@ -26,7 +26,7 @@ class Hasher final {
 
  private:
   const std::string key_;
-  core::crypto::HMAC_SHA256 hmac_;
+  core::mac::HMAC_SHA256 hmac_;
   const std::string headers_;
 };
 

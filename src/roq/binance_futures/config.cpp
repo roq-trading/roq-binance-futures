697de5c645bb500c8a65121a87869f158ae2e891
thraneh <thraneh@gmail.com>
2023-10-12 03:45:09 +0200

    rename Filter (from CancelAllOrdersFilter)

--- src/roq/binance_futures/config.cpp
+++ src/roq/binance_futures/config.cpp
@@ -29,7 +29,7 @@ auto const SUPPORTS = Mask{
 auto const MBP_ALLOW_REMOVE_NON_EXISTING = true;
 auto const OMS_REQUEST_ID_TYPE = RequestIdType::BASE64;
 auto const OMS_CANCEL_ALL_ORDERS = Mask{
-    CancelAllOrdersFilter::ACCOUNT,
+    Filter::ACCOUNT,
 };
 }  // namespace
 

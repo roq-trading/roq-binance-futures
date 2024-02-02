128d525cf2869558b7371cbce5cb3247d7077601
thraneh <thraneh@gmail.com>
2024-02-02 10:02:06 +0100

    depends on roq-market

--- src/roq/binance_futures/shared.hpp
+++ src/roq/binance_futures/shared.hpp
@@ -17,7 +17,7 @@
 
 #include "roq/core/limit/rate_limiter.hpp"
 
-#include "roq/utils/mbp/sequencer.hpp"
+#include "roq/market/mbp/sequencer.hpp"
 
 #include "roq/binance_futures/api.hpp"
 #include "roq/binance_futures/settings.hpp"
@@ -64,7 +64,7 @@ struct Shared final {
   struct Instrument final {
     int64_t tob_last_update_id = {};
     int64_t mbp_last_update_id = {};
-    utils::mbp::Sequencer sequencer;
+    market::mbp::Sequencer sequencer;
 
     bool tob_update(int64_t update_id) {
       if (update_id < tob_last_update_id)

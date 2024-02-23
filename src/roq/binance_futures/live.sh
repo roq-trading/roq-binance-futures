8785ebf8393d722f0f5fa6b29214db6c9fe73186
thraneh <thraneh@gmail.com>
2024-02-23 15:30:53 +0100

    update

--- src/roq/binance_futures/live.sh
+++ src/roq/binance_futures/live.sh
@@ -35,7 +35,6 @@ $PREFIX ./roq-binance-futures \
   --rest_uri "$REST_URI" \
   --ws_pm_uri "$WS_PM_URI" \
   --rest_pm_uri "$REST_PM_URI" \
-  --enable_portfolio_margin=true \
   --download_symbols="BTCUSDT,ETHUSDT" \
   --download_trades_lookback=300s \
   $@

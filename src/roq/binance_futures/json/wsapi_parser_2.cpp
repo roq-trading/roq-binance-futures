289a5b3443d12e476541cd839e76a5aa3727d2bb
thraneh <thraneh@gmail.com>
2024-04-24 09:37:32 +0200

    chore

--- src/roq/binance_futures/json/wsapi_parser_2.cpp
+++ src/roq/binance_futures/json/wsapi_parser_2.cpp
@@ -29,57 +29,10 @@ auto get_request(auto &message) -> WSAPIRequest {
   return {};
 }
 
-bool dispatch_listen_key(auto &handler, auto &message, auto &buffer, auto &trace_info, auto &request) {
-  WSAPIListenKey listen_key{message, buffer};
-  create_trace_and_dispatch(handler, trace_info, listen_key, request);
-  return true;
-}
-
-bool dispatch_account_balance(auto &handler, auto &message, auto &buffer, auto &trace_info, auto &request) {
-  WSAPIAccountBalance account{message, buffer};
-  create_trace_and_dispatch(handler, trace_info, account, request);
-  return true;
-}
-
-bool dispatch_account_status(auto &handler, auto &message, auto &buffer, auto &trace_info, auto &request) {
-  WSAPIAccountStatus account{message, buffer};
-  create_trace_and_dispatch(handler, trace_info, account, request);
-  return true;
-}
-
-bool dispatch_open_orders_status(auto &handler, auto &message, auto &buffer, auto &trace_info, auto &request) {
-  WSAPIOpenOrders open_orders{message, buffer};
-  create_trace_and_dispatch(handler, trace_info, open_orders, request);
-  return true;
-}
-
-bool dispatch_my_trades(auto &handler, auto &message, auto &buffer, auto &trace_info, auto &request) {
-  WSAPITrades trades{message, buffer};
-  create_trace_and_dispatch(handler, trace_info, trades, request);
-  return true;
-}
-
-bool dispatch_open_orders_cancel_all(auto &handler, auto &message, auto &buffer, auto &trace_info, auto &request) {
-  WSAPICancelOpenOrders cancel_open_orders{message, buffer};
-  create_trace_and_dispatch(handler, trace_info, cancel_open_orders, request);
-  return true;
-}
-
-bool dispatch_order_place(auto &handler, auto &message, auto &buffer, auto &trace_info, auto &request) {
-  WSAPIOrderPlace order_place{message, buffer};
-  create_trace_and_dispatch(handler, trace_info, order_place, request);
-  return true;
-}
-
-bool dispatch_order_modify(auto &handler, auto &message, auto &buffer, auto &trace_info, auto &request) {
-  WSAPIModifyOrder modify_order{message, buffer};
-  create_trace_and_dispatch(handler, trace_info, modify_order, request);
-  return true;
-}
-
-bool dispatch_order_cancel(auto &handler, auto &message, auto &buffer, auto &trace_info, auto &request) {
-  WSAPICancelOrder cancel_order{message, buffer};
-  create_trace_and_dispatch(handler, trace_info, cancel_order, request);
+template <typename T>
+bool dispatch_helper(auto &handler, auto &message, auto &buffer, auto &trace_info, auto &request) {
+  T obj{message, buffer};
+  create_trace_and_dispatch(handler, trace_info, obj, request);
   return true;
 }
 }  // namespace
@@ -96,25 +49,25 @@ bool WSAPIParser2::dispatch(
     case UNKNOWN__:
       break;
     case LISTEN_KEY_CREATE:
-      return dispatch_listen_key(handler, message, buffer, trace_info, request);
+      return dispatch_helper<WSAPIListenKey>(handler, message, buffer, trace_info, request);
     case LISTEN_KEY_PING:
       return true;  // note!
     case ACCOUNT_BALANCE:
-      return dispatch_account_balance(handler, message, buffer, trace_info, request);
+      return dispatch_helper<WSAPIAccountBalance>(handler, message, buffer, trace_info, request);
     case ACCOUNT_STATUS:
-      return dispatch_account_status(handler, message, buffer, trace_info, request);
+      return dispatch_helper<WSAPIAccountStatus>(handler, message, buffer, trace_info, request);
     case OPEN_ORDERS_STATUS:
-      return dispatch_open_orders_status(handler, message, buffer, trace_info, request);
+      return dispatch_helper<WSAPIOpenOrders>(handler, message, buffer, trace_info, request);
     case MY_TRADES:
-      return dispatch_my_trades(handler, message, buffer, trace_info, request);
+      return dispatch_helper<WSAPITrades>(handler, message, buffer, trace_info, request);
     case OPEN_ORDERS_CANCEL_ALL:
-      return dispatch_open_orders_cancel_all(handler, message, buffer, trace_info, request);
+      return dispatch_helper<WSAPICancelOpenOrders>(handler, message, buffer, trace_info, request);
     case ORDER_PLACE:
-      return dispatch_order_place(handler, message, buffer, trace_info, request);
+      return dispatch_helper<WSAPIOrderPlace>(handler, message, buffer, trace_info, request);
     case ORDER_MODIFY:
-      return dispatch_order_modify(handler, message, buffer, trace_info, request);
+      return dispatch_helper<WSAPIModifyOrder>(handler, message, buffer, trace_info, request);
     case ORDER_CANCEL:
-      return dispatch_order_cancel(handler, message, buffer, trace_info, request);
+      return dispatch_helper<WSAPICancelOrder>(handler, message, buffer, trace_info, request);
   }
   return false;
 }

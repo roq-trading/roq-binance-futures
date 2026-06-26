45fff2844b73b61666214d0d4fb2e4ef9822ebc9
thraneh <thraneh@gmail.com>
2026-06-26 10:29:47 +0000

    moving fix-bridge flags into roq-server

--- src/roq/binance_futures/fix_bridge/settings.cpp
+++ src/roq/binance_futures/fix_bridge/settings.cpp
@@ -2,19 +2,27 @@
 
 #include "roq/binance_futures/fix_bridge/settings.hpp"
 
+#include "roq/server/fix_bridge/flags/settings.hpp"
+
 using namespace std::literals;
 
 namespace roq {
 namespace binance_futures {
 namespace fix_bridge {
-namespace flags {
+
+// === HELPERS ===
+
+namespace {
+auto create_fix_bridge(auto &parser) {
+  return server::fix_bridge::flags::Settings{parser};
+}
+}  // namespace
 
 // === IMPLEMENTATION ===
 
-Settings::Settings(args::Parser const &parser) : binance_futures::flags::Settings{parser}, fix_bridge{flags::FIXBridge::create()} {
+Settings::Settings(args::Parser const &parser) : flags::Settings{parser}, fix_bridge{create_fix_bridge(parser)} {
 }
 
-}  // namespace flags
 }  // namespace fix_bridge
 }  // namespace binance_futures
 }  // namespace roq
